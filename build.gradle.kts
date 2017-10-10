4f81f7d2a69fd978c9b2c5cc81955fd1a169d7dc
Hany Elemary <hany.elemary@gmail.com>
2017-10-09 22:35:51 -0400

    Add support for spek and a BDD unit test for GitHubController

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,15 @@ buildscript {
 		mavenCentral()
 		maven("https://repo.spring.io/milestone")
 		maven("https://repo.spring.io/snapshot")
-	}
+        maven("http://dl.bintray.com/jetbrains/spek")
+    }
 
 	dependencies {
 		classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.0.BUILD-SNAPSHOT")
+        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.0")
+        classpath("org.jetbrains.spek:spek-api:1.1.5")
+        classpath("org.jetbrains.spek:spek-junit-platform-engine:1.1.5")
+        classpath("org.junit.platform:junit-platform-runner:1.0.0")
 	}
 }
 
@@ -23,6 +28,7 @@ apply {
 	plugin("kotlin-spring")
 	plugin("kotlin-jpa")
 	plugin("org.springframework.boot")
+    plugin("org.junit.platform.gradle.plugin")
 }
 
 version = "1.0.0-SNAPSHOT"
@@ -38,7 +44,8 @@ dependencies {
     compile("org.springframework:spring-web")
 	compile("org.jetbrains.kotlin:kotlin-stdlib")
 	compile("org.jetbrains.kotlin:kotlin-reflect")
-	testCompile("org.springframework.boot:spring-boot-starter-test")
-    testCompile("junit:junit")
+    testCompile("org.springframework.boot:spring-boot-starter-test")
+    testCompile("org.jetbrains.spek:spek-api:1.1.5")
+    testCompile("org.junit.platform:junit-platform-runner:1.0.1")
+    testRuntime("org.jetbrains.spek:spek-junit-platform-engine:1.1.5")
 }
-
---
+++ src/test/kotlin/github/GitHubControllerTests.kt
@@ -0,0 +1,29 @@
+package github
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+@RunWith(JUnitPlatform::class)
+object GitHubControllerTests : Spek({
+    describe("GitHubController methods") {
+        given("") {
+            val githubService = mock(GitHubService::class.java)
+            val githubController = GitHubController(gitHubService = githubService)
+            val username = "hanyelemary"
+
+            on("get repos by username") {
+                githubController.reposByUser(username)
+
+                it("should invoke service get repos by username") {
+                    verify(githubService).reposByUser(username)
+                }
+            }
+        }
+    }
+})
